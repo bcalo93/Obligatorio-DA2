c35d2c39d210457cae8d302ba52a580fff4009ac
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-06 00:11:28 -0300

    guard added

---
+++ IndicatorsManagerApp/src/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const token = this.authService.getToken();
+    console.log('TOKEN: ', token);
+    if (token) {
+      return true;
+    } else {
+      this.router.navigate(['/login'], {
+        queryParams: {
+          return: state.url
+        }
+      });
+      return false;
+    }
+  }
+}
