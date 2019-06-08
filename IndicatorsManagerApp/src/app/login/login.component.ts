8ad29dc181af3a87cb220d8ca0fc2fe91dbb5238
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-08 16:18:46 -0300

    new login implemented with styles

--- IndicatorsManagerApp/src/app/login/login.component.ts
+++ IndicatorsManagerApp/src/app/login/login.component.ts
@@ -1,23 +1,44 @@
-import { Component } from '@angular/core';
-import { AuthService } from 'src/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/services';
 import { Router } from '@angular/router';
+import { Credentials } from 'src/models';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  constructor(
-    private authService: AuthService,
-    private router: Router) {}
+export class LoginComponent implements OnInit {
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+  errorMessage: string;
+  constructor(private authService: AuthService, private router: Router) {}
 
-  login(form: any) {
-    const credentials = form.value;
+  ngOnInit() {
+    this.errorMessage = '';
+  }
+
+  login() {
+    const username = this.username.value;
+    const password = this.password.value;
+    const credentials = new Credentials(username, password);
     this.authService.login(credentials)
       .subscribe(
       () => this.router.navigate(['/']),
-      (error: any) => console.log(error)
+      (error: any) => this.errorMessage = error
     );
+  }
+
+  getUsernameErrorMessage() {
+    return this.username.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  isFormInvalid() {
+    return this.username.invalid || this.password.invalid;
   }
 }
