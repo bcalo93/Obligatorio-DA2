aff6842f3035b8e9ab8252e515547ce5e8b816e0
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-09 14:22:45 -0300

    user edit component added

--- IndicatorsManagerApp/src/app/user-edit/user-edit.component.ts
+++ IndicatorsManagerApp/src/app/user-edit/user-edit.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { UserService } from '../../services';
 import { User } from 'src/models';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
@@ -13,9 +13,18 @@ import { User } from 'src/models';
 export class UserEditComponent implements OnInit {
 
   user: User;
+  currentUserName: string;
+
+  name = new FormControl('', [Validators.required]);
+  lastName = new FormControl('', [Validators.required]);
+  username = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  roles = ['Admin', 'Manager'];
+  selectedRole: string;
+  errorMessage: string;
 
   constructor(
-    private route: ActivatedRoute,
     private userService: UserService,
     private location: Location) { }
 
@@ -24,11 +33,49 @@ export class UserEditComponent implements OnInit {
       .subscribe(users => {
         this.user = users[0];
         console.log(this.user);
+        this.currentUserName = this.user.name;
       });
+  }
+
+  save() {
+    const name = this.name.value;
+    const lastName = this.lastName.value;
+    const username = this.username.value;
+    const email = this.email.value;
+    const password = this.password.value;
+    const user = new User(name, lastName, username, email, this.selectedRole, password);
+    console.log(user);
+    this.userService.addUser(user)
+    .subscribe(
+      (data: User) => {
+        this.clearErrorMsg();
+        console.log(data);
+      },
+      (error: any) => {
+        this.errorMessage = error;
+        console.log(this.errorMessage);
+      }
+    );
   }
 
   goBack(): void {
     this.location.back();
   }
 
+  keyEnter() {
+    return this.isFormInvalid() ? null : this.save();
+  }
+
+  isFormInvalid() {
+    return this.name.invalid
+      || this.lastName.invalid
+      || this.username.invalid
+      || this.password.invalid
+      || this.email.invalid;
+  }
+
+  clearErrorMsg() {
+    this.errorMessage = '';
+  }
 }
+
