7c5b524ab935f6ee21fb494561defde3a4c5f424
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-16 09:53:50 -0300

    create area edit component

---
+++ IndicatorsManagerApp/src/app/area-edit/area-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-edit',
+  templateUrl: './area-edit.component.html',
+  styleUrls: ['./area-edit.component.css']
+})
+export class AreaEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
