29c536f3dd5dd87ec07ee0f341f3cdf111d773da
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-18 20:10:22 -0300

    dialog component created

---
+++ IndicatorsManagerApp/src/app/dialog/dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, Injectable } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { DialogData } from 'src/models/dialogData';
+
+@Injectable({
+  providedIn: 'root'
+})
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
+
+}
