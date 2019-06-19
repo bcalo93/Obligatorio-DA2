b68b7d58a7f57947c4c5c652759ed220fa26d52c
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-19 04:14:01 -0300

    indicator models updated

--- IndicatorsManagerApp/src/models/indicatorConfig.ts
+++ IndicatorsManagerApp/src/models/indicatorConfig.ts
@@ -3,4 +3,21 @@ export class IndicatorConfig {
     position: number;
     isVisible: boolean;
     alias: string;
+
+    constructor(indicatorConfig: any) {
+      this.indicatorId = indicatorConfig.id;
+      if (indicatorConfig.alias) {
+        this.alias = indicatorConfig.alias;
+      } else { this.alias = indicatorConfig.name; }
+      this.isVisible = indicatorConfig.isVisible;
+      this.position = indicatorConfig.position;
+    }
+
+    setPosition(position: number){
+      this.position = position;
+    }
+
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    }
 }
