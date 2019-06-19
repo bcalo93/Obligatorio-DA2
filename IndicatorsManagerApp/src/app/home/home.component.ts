0194b7ee32e1123dffe79ab348ba1ba8cb6befef
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-19 04:13:01 -0300

    home component updated

---
+++ IndicatorsManagerApp/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/services';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  isManagerLoggedIn() {
+    return this.authService.isManagerLoggedIn();
+  }
+
+  isAdminLoggedIn() {
+    return this.authService.isAdminLoggedIn();
+  }
+}
