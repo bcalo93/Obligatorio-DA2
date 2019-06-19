ef5521840a5ba7c0fade1870b19ab7e7e0173ecf
Brahian <bcalo93@gmail.com>
2019-06-19 17:44:19 -0300

    [GREEN] - Fixed Area delete with Indicators.

--- backend/IndicatorsManager.DataAccess/AreaRepository.cs
+++ backend/IndicatorsManager.DataAccess/AreaRepository.cs
@@ -6,12 +6,13 @@ using Microsoft.EntityFrameworkCore;
 using IndicatorsManager.DataAccess.Interface;
 using IndicatorsManager.DataAccess.Interface.Exceptions;
 using System.Data.SqlClient;
+using IndicatorsManager.DataAccess.Visitors;
 
 namespace IndicatorsManager.DataAccess
 {
     public class AreaRepository : BaseRepository<Area>, IAreaQuery
     {
-        public AreaRepository(DbContext context): base(context) { }
+        public AreaRepository(DbContext context) : base(context) { }
 
         public override Area Get(Guid id)
         {
@@ -19,19 +20,19 @@ namespace IndicatorsManager.DataAccess
             {
                 return context.Set<Area>().Where(x => x.Id == id).FirstOrDefault();
             }
-            catch(SqlException ex)
+            catch (SqlException ex)
             {
                 throw new DataAccessException(CONNECTION_ERROR, ex);
             }
         }
-        
+
         public override IEnumerable<Area> GetAll()
         {
             try
             {
                 return context.Set<Area>().ToList();
             }
-            catch(SqlException ex)
+            catch (SqlException ex)
             {
                 throw new DataAccessException(CONNECTION_ERROR, ex);
             }
@@ -43,10 +44,37 @@ namespace IndicatorsManager.DataAccess
             {
                 return this.context.Set<Area>().Where(a => a.Name == name).FirstOrDefault();
             }
-            catch(SqlException ex)
+            catch (SqlException ex)
             {
                 throw new DataAccessException(CONNECTION_ERROR, ex);
             }
+        }
+
+        public override void Remove(Area area)
+        {
+            try
+            {
+                if (area == null)
+                {
+                    throw new DataAccessException("El indicador es null.");
+                }
+                area.Indicators.ForEach(i => RemoveIndicator(i));
+                this.context.Set<Area>().Remove(area);
+            }
+            catch (SqlException ex)
+            {
+                throw new DataAccessException(CONNECTION_ERROR, ex);
+            }
+        }
+
+        private void RemoveIndicator(Indicator indicator)
+        {
+            foreach (IndicatorItem items in indicator.IndicatorItems)
+            {
+                this.context.Set<Component>().RemoveRange(items.Condition.Accept(
+                    new VisitorComponentToList()));
+            }
+            this.context.Set<Indicator>().Remove(indicator);
         }
 
     }
