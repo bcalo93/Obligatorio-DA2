b97d09607ab262ced92b5dfff19912a9d40af5d1
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-20 18:16:34 -0300

    log report completed

--- IndicatorsManagerApp/src/app/reports-log/reports-log.component.ts
+++ IndicatorsManagerApp/src/app/reports-log/reports-log.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models';
+import { ReportsService } from 'src/services';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reports-log',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsLogComponent implements OnInit {
 
-  constructor() { }
+  logs: Array<any> = [];
+  errorMessage = '';
+  startDate = new FormControl('', [Validators.required]);
+  endDate = new FormControl('', [Validators.required]);
+
+  constructor(private reportsService: ReportsService) { }
 
   ngOnInit() {
   }
 
+  transformDate(date: any) {
+    return date.toISOString().split('T')[0];
+  }
+
+  transformDateByString(date: string) {
+    return date.split('T')[0];
+  }
+
+
+  onChangeStartDate(event: any) {
+    let value = event.target.value;
+    if (value instanceof Date) {
+      value = value.toISOString().split('T')[0];
+    } else { this.startDate.setErrors({incorrect: true}); }
+  }
+
+  onChangeEndDate(event: any) {
+    let value = event.target.value;
+    if (value instanceof Date) {
+      value = value.toISOString().split('T')[0];
+    } else { this.endDate.setErrors({incorrect: true}); }
+  }
+
+
+  isValidDate() {
+    return this.endDate.valid && this.startDate.valid;
+  }
+
+  showLog(){
+
+    // console.log(this.startDate)
+    // console.log(this.endDate)
+    this.reportsService.getSystemActions(
+      this.transformDate(this.startDate.value),
+      this.transformDate(this.endDate.value)
+    ).subscribe(
+      response => {
+        this.logs = response;
+        console.log(this.logs);
+      },
+      error => this.errorMessage = error
+    );
+  }
 }
