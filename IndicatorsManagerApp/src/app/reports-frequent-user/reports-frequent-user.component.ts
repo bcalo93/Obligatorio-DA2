584e47ea85ac7e4967447fed4d41c83c5d62849b
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-20 16:58:21 -0300

    report frequent user component completed

---
+++ IndicatorsManagerApp/src/app/reports-frequent-user/reports-frequent-user.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models';
+import { ReportsService } from 'src/services';
+
+@Component({
+  selector: 'app-reports-frequent-user',
+  templateUrl: './reports-frequent-user.component.html',
+  styleUrls: ['./reports-frequent-user.component.css']
+})
+export class ReportsFrequentUserComponent implements OnInit {
+
+  users: Array<User> = [];
+  errorMessage = '';
+
+  constructor(private reportsService: ReportsService) { }
+
+  ngOnInit() {
+    this.reportsService.getTopFrequentUsers().subscribe(
+      response => {
+        this.users = response;
+        console.log(this.users);
+      },
+      error => this.errorMessage = error
+    );
+  }
+
+}
