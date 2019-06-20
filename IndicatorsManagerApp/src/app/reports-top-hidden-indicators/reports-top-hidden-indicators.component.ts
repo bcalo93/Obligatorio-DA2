27c5487ed1285988f2f7687d4c5f56564bd9bffe
Ramiro Gonzalez <rgonzalez@altimetrik.com>
2019-06-20 17:00:38 -0300

    first commit component

---
+++ IndicatorsManagerApp/src/app/reports-top-hidden-indicators/reports-top-hidden-indicators.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-top-hidden-indicators',
+  templateUrl: './reports-top-hidden-indicators.component.html',
+  styleUrls: ['./reports-top-hidden-indicators.component.css']
+})
+export class ReportsTopHiddenIndicatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
